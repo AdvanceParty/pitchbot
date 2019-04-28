7782d23b48466cf3df2f1d52653679f31401e619
Gil Fewster <hello@gilfewster.com>
2019-04-28 20:54:31 +1000

    tidied js funcs

--- src/index.js
+++ src/index.js
@@ -2,25 +2,33 @@ const ENDPOINTS = {
   GET_TITLE: '/.netlify/functions/getTitle',
 };
 
-const init = () => {
-  console.log('Running');
-  const wrapper = document.querySelector('#pitch');
-  const inner = document.querySelector('#pitchContent');
-  getTitle(wrapper, inner);
+const init = () => {  
+  getTitle();
 };
 
 const getTitle = async (wrapper, inner) => {
-  wrapper.classList.add('thinking');
-
-  const response = await callAPI(ENDPOINTS.GET_TITLE);
-  const data = await response.json();
-
+  const inner = document.querySelector('#pitchContent');
+  const data = await callAPI(ENDPOINTS.GET_TITLE);
   inner.innerHTML = data;
-  console.log(data);
 };
 
 const callAPI = endpoint => {
-  return fetch(endpoint);
+  setLoading(true);
+  let message;
+  try {
+    const response = await fetch(endpoint);
+    message = await response.json();
+  } catch (e) {
+    message = "Something went wrong. Oops."
+  } finally {
+    setLoading(false);
+    return data;
+  }
 };
+
+const setLoading = (isLoading) => {
+  const wrapper = document.querySelector('#pitch');
+  isLoading ? wrapper.classList.add('loading') : wrapper.classList.remove('loading')
+}
 
 init();
